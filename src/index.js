492681041cec3a042dd8ee1646e3fed7fad00088
Matheus Barão Fiorin <matheusfiiorin@gmail.com>
2020-05-14 12:48:24 -0300

    Refactor index.js to a better structure

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import * as core from "./core.js";
 import * as fs from "fs";
 import { apiKey, transaction } from "./example.js";
 
-try {
-  core.connectClient(apiKey).then((client) => {
+core
+  .connectClient(apiKey)
+  .then((client) => {
     console.info("[index] client created");
     core.createTransaction(client, transaction).then((transaction) => {
       console.info("[index] transaction created");
       fs.writeFileSync(`./logs/transaction.json`, JSON.stringify(transaction));
       console.info("[index] transaction logged to logs/transaction.json");
     });
+  })
+  .catch((error) => {
+    console.log("[index] uncaught error:");
+    console.error(error);
   });
-} catch (error) {
-  console.log("[index] uncaught error:");
-  console.error(error);
-}
